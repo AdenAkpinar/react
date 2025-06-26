ee7f2d12f061705f2b702e1a629b98d840b8e1bf
Aden Akpınar <adenakpinar@gmail.com>
2025-06-26 03:31:55 +0300

    home sayfası eklendi ve basit bir girişle login sayfasından home sayfasına yönlendirildi.

--- train/src/App.js
+++ train/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login/Login";
-
-
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
